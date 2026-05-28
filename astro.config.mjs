07a946f57806fc7befc136196ee5fab8d37c1c05
exploriter <67083853+exploriter@users.noreply.github.com>
2026-05-28 07:23:26 -0700

    Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  integrations: [mdx(), react()],
+  adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
