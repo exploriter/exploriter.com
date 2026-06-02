868eb1a9fcf7351ef819e0868fc66481b7a01af7
exploriter <67083853+exploriter@users.noreply.github.com>
2026-06-02 12:29:13 -0700

    Update Callout.tsx

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -7,15 +7,24 @@ type CalloutProps = {
    className?: string;
 };
 
+type CalloutGridProps = {
+   children: ReactNode;
+   className?: string;
+};
+
 export function Callout({ children, title, className }: CalloutProps) {
    return (
       <aside className={cn("border border-dashed bg-muted/50 px-5 py-4 pt-6 rounded my-12 relative", className)}>
          {title ? (
             <h3 className="font-mono text-xs font-medium uppercase tracking-wider text-muted-foreground bg-white rounded px-2 py-1 border border-dashed absolute -top-3.5 left-3">
-               {title}
+               <span dangerouslySetInnerHTML={{ __html: title }} />
             </h3>
          ) : null}
          <div className="text-base leading-6.5 text-foreground [&>p:first-child]:mt-0 [&>p:last-child]:mb-0">{children}</div>
       </aside>
    );
+}
+
+export function CalloutGrid({ children, className }: CalloutGridProps) {
+   return <div className={cn("grid gap-12 md:gap-6 mt-16 mb-12 md:grid-cols-2 [&>aside]:my-0", className)}>{children}</div>;
 }
